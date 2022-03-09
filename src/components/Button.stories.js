c150e1bc3ec3906cd3421963ee1a1b34cfb0ba2b
Jeremias Erbs <jerry@badabam.de>
2022-03-09 12:24:16 +0100

    Update Button stories

--- src/components/Button.stories.js
+++ src/components/Button.stories.js
@@ -13,12 +13,15 @@ const Template = args => <Button {...args} />;
 export const Default = Template.bind({});
 Default.args = {
   children: 'Click me',
+  isDark: false,
+  fullWidth: false,
 };
 
 export const Dark = Template.bind({});
 Dark.args = {
   children: 'Click me',
   isDark: true,
+  fullWidth: false,
 };
 export const FullWidth = Template.bind({});
 FullWidth.args = {
